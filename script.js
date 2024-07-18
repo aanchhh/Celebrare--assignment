e72393009860278c2d14106cf647ad23fbc9c771
AANCHAL MEHTA <144115215+aanchhh@users.noreply.github.com>
2024-07-18 22:59:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var quill = new Quill('#editor-container', {
+        modules: {
+            toolbar: '#toolbar-container'
+        },
+        theme: 'snow'
+    });
+
+    document.getElementById('add-text').onclick = function() {
+        quill.focus();
+    };
+
+    document.getElementById('undo').onclick = function() {
+        quill.history.undo();
+    };
+    document.getElementById('redo').onclick = function() {
+        quill.history.redo();
+    };
+});
