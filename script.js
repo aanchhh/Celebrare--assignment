6cc84762c0114fff144d7471289c870227d3e92f
AANCHAL MEHTA <144115215+aanchhh@users.noreply.github.com>
2024-07-18 23:49:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var quill = new Quill('#editor-container', {
-        modules: {
-            toolbar: '#toolbar-container'
-        },
-        theme: 'snow'
-    });
+var quill = new Quill('#editor-container', {
+    modules: {
+        toolbar: '#toolbar-container'
+    },
+    theme: 'snow'
+});
 
-    document.getElementById('add-text').onclick = function() {
-        quill.focus();
-    };
+document.getElementById('add-text').onclick = function() {
+    quill.focus();
+};
 
-    document.getElementById('undo').onclick = function() {
-        quill.history.undo();
-    };
-    document.getElementById('redo').onclick = function() {
-        quill.history.redo();
-    };
+document.getElementById('undo').onclick = function() {
+    quill.history.undo();
+};
+
+document.getElementById('redo').onclick = function() {
+    quill.history.redo();
+};
+
+let draggedText = '';
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+quill.root.addEventListener('mousedown', function(e) {
+    const range = quill.getSelection();
+    if (range && range.length > 0) {
+        draggedText = quill.getText(range.index, range.length);
+        quill.deleteText(range.index, range.length);
+
+        const span = document.createElement('span');
+        span.classList.add('draggable-text');
+        span.innerText = draggedText;
+        document.getElementById('editor-container').appendChild(span);
+
+        span.style.left = e.clientX + 'px';
+        span.style.top = e.clientY + 'px';
+
+        isDragging = true;
+        offsetX = e.offsetX;
+        offsetY = e.offsetY;
+
+        span.addEventListener('mousedown', function(e) {
+            isDragging = true;
+            offsetX = e.offsetX;
+            offsetY = e.offsetY;
+        });
+    }
+});
+
+document.addEventListener('mouseup', function() {
+    isDragging = false;
+});
+
+document.addEventListener('mousemove', function(e) {
+    if (isDragging && draggedText) {
+        const span = document.querySelector('.draggable-text');
+        if (span) {
+            span.style.left = (e.clientX - offsetX) + 'px';
+            span.style.top = (e.clientY - offsetY) + 'px';
+        }
+    }
 });
